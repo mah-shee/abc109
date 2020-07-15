00a1c2fa7fa5d2ce1913478283ffcd5754888bce
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-15 16:30:25 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,8 +1,17 @@
+use num::integer::gcd;
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        x: usize,
+        a: [usize; n]
     }
+    let mut res = 0;
+    for i in a.iter() {
+        res = gcd(res, (x as isize - *i as isize).abs() as usize);
+    }
+    println!("{}", res);
 }
